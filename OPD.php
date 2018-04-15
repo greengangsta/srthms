30c8c0fe263c2c5519bb3bb79b670fc84fd91bf5
Suraj Tripathi <acc.surajtripathi@gmail.com>
2018-04-15 19:30:08 +0530

    deleted php folder ,there were repeated files.

--- OPD.php
+++ OPD.php
@@ -1,14 +1,22 @@
-<form>
-Registration :<br>
-Visit S/N :<input type="text" name="visit"><br>
-Reg Number :<input type="text" name="reg"><br>
-Department :<input type="text" name="department"><br>
-Consulting Doctor Name :<input type="text" name="doctor"><br>
-Patients Name :<input type="text" name="visit"><br>
-Fathers Name :<input type="text" name="father"><br>
-Age :<input type="text" name="age"> Sex :<input type="text" name="sex"> Religion :<input type="text" name="religion"><br>
-Address :<input type="text" name="address"><br>
-City :<input type="text" name="city">State :<input type="text" name="state"><br>
-Pin :<input type="text" name="pin">Phone No :<input type="text" name="phone"><br>
+<h1 align="center">OPD</h1>
+<hr>
+
+<form action="OPD.php" method="POST">
+<h2>Registration :</h2><br><br>
+Visit S/N :<input type="text" name="visit" required/><br><br>
+Reg Number :<input type="text" name="reg" required/><br><br>
+Department :<input type="text" name="department" required/><br><br>
+Consulting Doctor Name :<input type="text" name="doctor" required/><br><br>
+Patients Name :<input type="text" name="visit" required/><br><br>
+Fathers Name :<input type="text" name="father" required/><br><br>
+Age :<input type="text" name="age" required/>  Sex  :<input type="text" name="sex" required/>  Religion :<input type="text" name="religion" required/><br><br>
+Address :<input type="text" name="address" required/><br><br>
+City :<input type="text" name="city" required/>State :<input type="text" name="state" required/><br><br>
+Pin :<input type="text" name="pin" required/>Phone No :<input type="text" name="phone" required/><br><br>
 <input type="submit" value="Register">
 </form>
+    <style>
+    body{
+     background-color: LightSteelBlue;
+    }
+    </style>
--- PHP/reports.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-disposition: attachment; filename=srtreportmed.pdf');
-header('Content-type: application/pdf');
-readfile('srtreportmed.pdf');
-?>
